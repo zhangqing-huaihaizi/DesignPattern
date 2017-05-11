6f38af7b66c4238a83296b57702f025228282b0b
e <3110103952@zju.edu.cn>
2017-05-11 14:58:54 +0800

    Facade Pattern

---
+++ FacadePattern/FacadePattern/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacadePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.MethodA();
+            facade.MethodB();
+            Console.Read();
+        }
+    }
+    class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine("子系统方法一");
+        }
+    }
+    class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine("子系统方法二");
+        }
+    }
+    class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine("子系统方法三");
+        }
+    }
+    class SubSystemFour
+    {
+        public void MethodFour()
+        {
+            Console.WriteLine("子系统方法四");
+        }
+    }
+    class Facade
+    {
+        SubSystemOne one;
+        SubSystemTwo two;
+        SubSystemThree three;
+        SubSystemFour four;
+
+        public Facade(){
+            one = new SubSystemOne();
+            two = new SubSystemTwo();
+            three = new SubSystemThree();
+            four = new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("\n方法组A()----");
+            one.MethodOne();
+            two.MethodTwo();
+            four.MethodFour();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("\n方法组B()----");
+            two.MethodTwo();
+            three.MethodThree();
+        }
+    }
+}
